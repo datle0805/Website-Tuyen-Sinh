848710f1da88eae7cd414b024a889f132cda0781
datlt <datle0805@gmail.com>
2026-02-09 19:49:36 +0700

    add BE deploy vercel setup

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,17 +1,38 @@
 import mongoose from "mongoose";
 
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+    cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/admissions", {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
             serverSelectionTimeoutMS: 5000,
             socketTimeoutMS: 45000,
-        });
+        };
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${(error as Error).message}`);
-        process.exit(1);
+        cached.promise = mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/admissions", opts).then((mongoose) => {
+            console.log(`MongoDB Connected: ${mongoose.connection.host}`);
+            return mongoose;
+        });
     }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+
+    return cached.conn;
 };
 
 export default connectDB;
